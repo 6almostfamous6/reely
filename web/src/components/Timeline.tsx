391d7416345ab501392f2ce4a65fd2967aa3e160
6almostfamous6 <6almostfamous6@gmail.com>
2026-01-08 02:52:02 -0600

    Update Timeline.tsx

--- web/src/components/Timeline.tsx
+++ web/src/components/Timeline.tsx
@@ -1,1 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import PostCard from './PostCard';
+
+export default function Timeline() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/posts')
+      .then(res => res.json())
+      .then(setPosts);
+  }, []);
+
+  return (
+    <div>
+      {posts.map(post => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </div>
+  );
+}
 
