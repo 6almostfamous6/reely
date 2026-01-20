74a7ce8c179de1724b00e8dfd289e79aad06e6ed
6almostfamous6 <6almostfamous6@gmail.com>
2026-01-20 14:45:57 -0600

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import roomsRoutes from "./rooms/rooms.routes";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use("/rooms", roomsRoutes);
 
 type Post = {
   id: number;
