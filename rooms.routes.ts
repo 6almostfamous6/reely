9cfaa093f4177ebe4a9102e533d56677069abf5f
6almostfamous6 <6almostfamous6@gmail.com>
2026-01-20 14:42:30 -0600

    Update rooms.routes.ts

--- rooms.routes.ts
+++ rooms.routes.ts
@@ -1,1 +1,15 @@
+import { Router } from "express";
+import {
+  createRoomHandler,
+  getRoomHandler,
+  updateRoomHandler
+} from "./rooms.controller";
+
+const router = Router();
+
+router.post("/", createRoomHandler);
+router.get("/:id", getRoomHandler);
+router.patch("/:id", updateRoomHandler);
+
+export default router;
 
