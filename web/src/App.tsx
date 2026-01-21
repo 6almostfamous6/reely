ca5608aa57792bec931b5199888942fc7c9dda72
6almostfamous6 <6almostfamous6@gmail.com>
2026-01-21 15:01:55 -0600

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Room from "./pages/Room";
 
 export default function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/room/:id" element={<Room />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
